aa7a814c268a4502b24bd2aac3ab5dc06114ebf8
lanqiu1986 <lisuji1986@163.com>
2017-03-28 15:00:20 +0800

    complete common compute

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -22,6 +22,8 @@ class Parking < ApplicationRecord
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
       if duration <= 60
         self.amount = 200
+      else
+        self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
       end
     end
   end
--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -32,5 +32,26 @@ RSpec.describe Parking, type: :model do
       parking.calculate_amount
       expect(parking.amount).to eq(200)
     end
+
+    it "30 mins should be 2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 61.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(300)
+    end
+
+    it "30 mins should be 2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 90.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(300)
+    end
+
+    it "30 mins should be 2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 120.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(400)
+    end
   end
 end
